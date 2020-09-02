c047a981d0875f2cd4c272a5794122d4f9e452fd
wuqi <278600844@qq.com>
2020-09-02 21:28:15 +0800

    新建仓库

---
+++ src/main/java/com/example/Demogit001Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demogit001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demogit001Application.class, args);
+    }
+
+}
